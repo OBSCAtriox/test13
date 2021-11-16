f14860f17c637d5eb7e9ba5d13eecbaacf45b16a
OBSCAtriox <94022121+OBSCAtriox@users.noreply.github.com>
2021-11-16 08:26:47 +0000

    Add files via upload

---
+++ aula1/aula1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string charactername = "Jackie";
+            int characterAge = 35;
+            double decimalnumber = 5.99;
+            bool raining = false;
+
+            Console.WriteLine("Era uma vez uma vaca");
+            Console.WriteLine("Ela tinha " + characterAge);
+            charactername = "Michel";
+            Console.WriteLine("Ele adorou o nome" + charactername);
+            Console.WriteLine("Mas não gostou de ter" + characterAge);
+            Console.WriteLine(decimalnumber);
+
+            Console.ReadLine();
+;
+        }
+    }
+}
